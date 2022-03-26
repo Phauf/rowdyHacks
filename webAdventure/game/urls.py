eb88a841cda22031ad25b0edebbe6dd73a1c8774
SpinyKeyboard88 <juan.porraz84@gmail.com>
2022-03-26 17:56:58 -0500

    moved assets to static folder in Django

--- webAdventure/game/urls.py
+++ webAdventure/game/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+#paths for the urls
 urlpatterns = [
     path("", views.title)
 ]
--- webAdventure/game/views.py
+++ webAdventure/game/views.py
@@ -2,8 +2,29 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+from django.http import HttpResponse  # i think these are not needed
+from django.template import loader  # i think these are not needed
 
+welcome = [
+    {'title': "Starting out", 'description': "Here is a long description for the page",
+        'yousee': ['stick', 'map', 'potion']
+     },
+    {
+        'title': "The Wrestling room", 'description': "I DON'T WANT TO TYPE ANYMORE", 'yousee': ['sword', 'chair', 'enemy']
+    },
+    {
+        'title': "The Room", 'description': "A strange futuristic vibe fills the room", 'yousee': ['monitor', 'table', 'computer']
+    }
+]
+
+print(welcome[2]['title'] + "\n" + welcome[2]
+      ['description'] + "\n>You see: \n" + ', '.join(welcome[2]['yousee']))
+
+
+# this will
 def title(request):
-    return render(request, "game/title.html")
+    return render(request, "game/title.html", )
+
+
+def some(request):
+    return render(request, )
