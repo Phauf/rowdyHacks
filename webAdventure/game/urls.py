26785b49659fadb2de851911162fb4c0066f857c
Marcus Dehoyos <bennydehoyos1129@gmail.com>
2022-03-26 23:40:52 -0500

    cryingg

--- webAdventure/game/urls.py
+++ webAdventure/game/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 
 # paths for the urls
+
 urlpatterns = [
     path("", views.title),
     # you have to put world in the url, but this can be changed
     # when you click start, which should do that
-    path("world", views.world)
+    path("world", views.world, name="world")
 ]
