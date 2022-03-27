2a00b84a84d4a335666976e139c338db788f0c4f
SpinyKeyboard88 <juan.porraz84@gmail.com>
2022-03-26 20:01:02 -0500

    made world.html, url in urls.py, dicts in views.py
    Added the dictionary values in views.py which also sets the variables to be used in world.html. It should be changed depending on which direction you choose.
    title.html has a start button that doesn't work, but it should take you to the first world page.
    Added a url path in urls.py

--- webAdventure/game/urls.py
+++ webAdventure/game/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
-#paths for the urls
+# paths for the urls
 urlpatterns = [
-    path("", views.title)
+    path("", views.title),
+    # you have to put world in the url, but this can be changed
+    # when you click start, which should do that
+    path("world", views.world)
 ]
--- webAdventure/game/views.py
+++ webAdventure/game/views.py
@@ -1,3 +1,4 @@
+from optparse import TitledHelpFormatter
 from django.shortcuts import render
 
 # Create your views here.
@@ -5,26 +6,42 @@ from django.shortcuts import render
 from django.http import HttpResponse  # i think these are not needed
 from django.template import loader  # i think these are not needed
 
+# there will be images with same format in the visual value
+# more dictionary values will be added
+# 49 more...
 welcome = [
-    {'title': "Starting out", 'description': "Here is a long description for the page",
+    {'visual': "img-6-3", 'title': "Starting out", 'description': "Here is a long description for the page",
         'yousee': ['stick', 'map', 'potion']
      },
     {
-        'title': "The Wrestling room", 'description': "I DON'T WANT TO TYPE ANYMORE", 'yousee': ['sword', 'chair', 'enemy']
+        'visual': "img-6-2", 'title': "The Wrestling room", 'description': "I DON'T WANT TO TYPE ANYMORE", 'yousee': ['sword', 'chair', 'enemy']
     },
     {
         'title': "The Room", 'description': "A strange futuristic vibe fills the room", 'yousee': ['monitor', 'table', 'computer']
     }
 ]
 
-print(welcome[2]['title'] + "\n" + welcome[2]
-      ['description'] + "\n>You see: \n" + ', '.join(welcome[2]['yousee']))
 
-
-# this will
+# This will show the title.html page
 def title(request):
-    return render(request, "game/title.html", )
+    return render(request, "game/title.html")
 
 
-def some(request):
-    return render(request, )
+def world(request):
+    # This should be checked
+    # welcome[changeMeDependingOnPosition][0]
+    # welcome[changeMeDependingOnPosition][1]
+    # welcome[changeMeDependingOnPosition][2]
+    visual = welcome[0]['visual']
+    title = welcome[0]['title']
+    description = welcome[0]['description']
+    yousee = welcome[0]['yousee']
+
+# These are the variables that are sent to the html document
+    context = {
+        "visual": visual,
+        "title": title,
+        "description": description,
+        "yousee": yousee
+    }
+    return render(request, "game/world.html", context)
