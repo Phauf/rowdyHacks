7876648f765bb3c274d23482f0e1b6a15be054b0
SpinyKeyboard88 <juan.porraz84@gmail.com>
2022-03-26 23:41:43 -0500

    added dice and shaking gifs, made a battle.html

--- webAdventure/game/urls.py
+++ webAdventure/game/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.title),
     # you have to put world in the url, but this can be changed
     # when you click start, which should do that
-    path("world", views.world)
+    path("world", views.world, name='world'),
+    path("battle", views.battle, name='battle')
 ]
